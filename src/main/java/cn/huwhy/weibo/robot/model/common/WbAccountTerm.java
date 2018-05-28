e6918a772c0c219862ac715032cc7d45f85f9346
huwhy <huwhy@foxmail.com>
2018-05-28 15:34:44 +0800

    v1.1

--- src/main/java/cn/huwhy/weibo/robot/model/common/WbAccountTerm.java
+++ src/main/java/cn/huwhy/weibo/robot/model/common/WbAccountTerm.java
@@ -6,11 +6,21 @@ public class WbAccountTerm extends Term {
 
     private String username;
 
+    private int memberId;
+
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
     }
 }
